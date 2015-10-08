4108c9028244debe1c8e3915648b3c78aa164408
vagrant <vagrant@homestead>
2015-10-08 05:31:56 +0000

    creacion del proyecto

---
+++ database/migrations/2015_10_08_044328_create_products_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateProductsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('produts', function(Blueprint $table) {
+            $table->increments('id');
+            $table->string('name');
+            $table->float('price');
+            $table->dateTime('created_at');
+            $table->dateTime('update_at');
+            $table->dateTime('deleted_at');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('products');
+    }
+}
