15981dc0677f43aa8243074b2cc52b8648815e72
vagrant <vagrant@homestead>
2015-10-17 00:12:28 +0000

    instalacion helper HTML, integrando blade a vistas

--- resources/views/product/list.blade.php
+++ resources/views/product/list.blade.php
@@ -1,9 +1,27 @@
 <html>
-<head><title>Catálogo</title></head>
+
+<head>
+    <title>Logi</title>
+    {!! HTML::style('css/products.css') !!}
+</head>
 
 <body>
 
-mi primera vista
+<h1>Lista de Productos</h1>
+
+@if (count($products))
+<ul>
+    @foreach( $products as $product )
+    <li>
+        <article>
+            <header>{{ $product->name  }}</header>
+            <span id="price">{{ $product->price  }}</span>
+        </article>
+
+    </li>
+    @endforeach
+</ul>
+@endif
 
 </body>
 </html>
